dce34722fe0d6f829ec297fe3f6da55f1e87fbde
ABHI <physicsevil007@gmail.com>
2026-08-25 01:32:52 +0530

    ok
    ok

--- main.java
+++ main.java
@@ -1,3 +1,10 @@
-public class main {
-    
+package ".pack";
+public class Main {
+    public static void main(String[] args) { //basically the string[] is a command line array means in terminal we pass the value of the array "java Main 30"
+        System.out.println(args[1]); // it will return 30
+    }
 }
+
+//javac -> is a executeable file stored in our pc. it create a .class file(byte code) of the .java file we give to the java compiler
+
+// package ->it is a folder in which the java file is present
