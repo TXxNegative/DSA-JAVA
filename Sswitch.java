80f2122926b98529292f531e8f396b83dd11ec63
ABHI <physicsevil007@gmail.com>
2026-08-26 16:57:56 +0530

    basics

---
+++ Sswitch.java
@@ -0,0 +1,49 @@
+
+import java.util.Scanner;
+
+public class Sswitch {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        String fruit=input.next();
+        int day=input.nextInt();
+        //normal  switch
+        /* 
+        switch (fruit) {
+            case "mango":
+                System.out.println("the king of the fruits");
+                break;
+            case"litchi":
+                System.out.println("red spikey outer cover,seed inside");
+                break;
+            case"banana":
+                System.out.println("most consumed fruit in the world");
+                break;
+            default:
+                System.out.println("fruit is not in store");
+        }
+        */
+        
+        //enhanced switch
+         
+        switch (fruit) {
+            case "mango" -> System.out.println("the king of the fruits");
+            case"litchi" -> System.out.println("red spikey outer cover,seed inside");
+            case"banana" -> System.out.println("most consumed fruit in the world");
+            default -> System.out.println("fruit is not in store");
+        }
+        
+
+        switch(day){
+            case 1 -> System.out.println("monday");
+            case 2 -> System.out.println("tuesday");
+            case 3 -> System.out.println("wednesday");
+            case 4 -> System.out.println("thursday");
+            case 5 -> System.out.println("friday");
+            case 6 -> System.out.println("saturday");
+            case 7 -> System.out.println("sunday");
+        }
+        
+        //nested switch
+        
+    }
+}
---
+++ loop.java
@@ -0,0 +1,20 @@
+public class loop {
+    public static void main(String[] args) {
+        //while loop
+        
+        int i=0;
+        while(i<10){
+            System.out.println(i);
+            if(i==7){break;}// break will stopthe whole process 
+            i=i+1;
+        }
+        System.out.println("out of everything");
+
+        // for loop
+        for(int a=0;a<10;a++){
+            System.out.println(i);
+        }
+
+        // jab hame pata hai ki kitne bar hame loop chalana hai tab hum for llop use karte hai, jab hame pata nahi hota kitna bar llop chalana hai tab hum whle use karte hai
+    }
+}
